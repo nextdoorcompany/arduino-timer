404dcfac298bcaf0a9702e0493258b5b987ab5a0
Justin Schechter <justin@nextdoorco.com>
2016-07-23 18:54:42 -0400

    Add basic timer

---
+++ Timer/Timer.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
+
+Adafruit_7segment matrix = Adafruit_7segment();
+int buttonPin = 9;
+int counter = 0;
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  matrix.begin(0x70);  
+}
+
+void loop() {
+  if (digitalRead(buttonPin) == LOW) {
+    matrix.print(counter);
+    matrix.writeDisplay();
+    counter++;
+    delay(1000);
+  } else {
+    matrix.clear();
+    matrix.writeDisplay();
+    counter = 0;
+  }
+
+}
