240619e8068986d391972c4c1d2042287a43263f
Justin Schechter <justin@nextdoorco.com>
2016-07-27 19:42:58 -0400

    Add second switch and update for 1.2 display

--- Timer/Timer.ino
+++ Timer/Timer.ino
@@ -1,5 +1,6 @@
-/* 7 segment + to 5V, - to GND, D to A4, C to A5 
- * switch D9 to GND
+/* 7 segment + to 5V, - to GND, D to SDA, C to SCL, IO jump to +
+ * switch 1 D9 to GND
+ * switch 2 D8 to GND
  */
 
 #include <Wire.h>
@@ -7,17 +8,19 @@
 #include "Adafruit_GFX.h"
 
 Adafruit_7segment matrix = Adafruit_7segment();
-int buttonPin = 9;
+int switchOne = 9;
+int switchTwo = 8;
 int seconds = 0;
 int minutes = 0;
 
 void setup() {
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(switchOne, INPUT_PULLUP);
+  pinMode(switchTwo, INPUT_PULLUP);
   matrix.begin(0x70);  
 }
 
 void loop() {
-  if (digitalRead(buttonPin) == LOW) {
+  if ((digitalRead(switchOne) == LOW) || (digitalRead(switchTwo) == LOW)) {
     matrix.drawColon(true);
     matrix.writeDigitNum(4, seconds % 10);
     matrix.writeDigitNum(3, seconds / 10);
@@ -25,9 +28,12 @@ void loop() {
     matrix.writeDigitNum(0, minutes / 10);
     matrix.writeDisplay();
     seconds++;
-    if (seconds == 60) {
+    if (seconds > 59) {
       seconds = 0;
       minutes++;
+    }
+    if (minutes > 99) {
+      minutes = 0;
     }
     delay(1000);
   } else {
