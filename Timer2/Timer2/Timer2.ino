7036c09b1a63cab5cc7ed74b85cc74f680ab8ae9
Justin Schechter <justin@nextdoorco.com>
2016-09-24 21:51:19 -0400

    Format clean up

--- Timer2/Timer2/Timer2.ino
+++ Timer2/Timer2/Timer2.ino
@@ -1,27 +1,3 @@
-/*
-  Repeating Web client
-
- This sketch connects to a a web server and makes a request
- using a Wiznet Ethernet shield. You can use the Arduino Ethernet shield, or
- the Adafruit Ethernet shield, either one will work, as long as it's got
- a Wiznet Ethernet module on board.
-
- This example uses DNS, by assigning the Ethernet client with a MAC address,
- IP address, and DNS address.
-
- Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
-
- created 19 Apr 2012
- by Tom Igoe
- modified 21 Jan 2014
- by Federico Vanzati
-
- http://arduino.cc/en/Tutorial/WebClientRepeating
- This code is in the public domain.
-
- */
-
 #include <SPI.h>
 #include <Ethernet2.h>
 #include <Wire.h>
@@ -32,81 +8,49 @@ Adafruit_7segment matrix = Adafruit_7segment();
 int seconds = 1;
 int minutes = 0;
 
-// assign a MAC address for the ethernet controller.
-// fill in your address here:
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
-// fill in an available IP address on your network here,
-// for manual configuration:
+
 IPAddress ip(192, 168, 88, 53);
 
-// fill in your Domain Name Server address here:
 IPAddress myDns(1, 1, 1, 1);
 
-// initialize the library instance:
 EthernetClient client;
 
-//char server[] = "www.arduino.cc";
-IPAddress server(64,131,82,241);
-
-unsigned long lastConnectionTime = 0;             // last time you connected to the server, in milliseconds
-const unsigned long postingInterval = 10L * 1000L; // delay between updates, in milliseconds
-// the "L" is needed to use long type numbers
+IPAddress server(64, 131, 82, 241);
 
 void setup() {
-  matrix.begin(0x70); 
-  Serial.begin(9600);
-  while (!Serial) {
-    ;
-  }
-  Serial.println("1");
-  
+  matrix.begin(0x70);
+
   // give the ethernet module time to boot up:
   delay(1000);
+
   // start the Ethernet connection using a fixed IP address and DNS server:
   Ethernet.begin(mac, ip, myDns);
-  // print the Ethernet board/shield's IP address:
-  Serial.println("2");
-  while(true) {
-    Serial.println(seconds);
-    writeLED(seconds, minutes);
-    seconds++;
-    if (seconds > 59) {
-      seconds = 0;
-      minutes++;
-    }
-    if (minutes > 99) {
-      minutes = 0;
-    }
-    delay(1000);
-  }
+  //httpRequest();
 }
 
 void loop() {
-  // if there's incoming data from the net connection.
-  // send it out the serial port.  This is for debugging
-  // purposes only:
-  if (client.available()) {
-    char c = client.read();
-    Serial.write(c);
+  writeLED(seconds, minutes);
+  seconds++;
+  if (seconds > 59) {
+    seconds = 0;
+    minutes++;
   }
-
-  // if ten seconds have passed since your last connection,
-  // then connect again and send data:
-  if (millis() - lastConnectionTime > postingInterval) {
-    httpRequest();
+  if (minutes > 99) {
+    minutes = 0;
   }
-
+  delay(1000);
 }
 
 void writeLED(int seconds, int minutes) {
-    matrix.drawColon(true);
-    matrix.writeDigitNum(4, seconds % 10);
-    matrix.writeDigitNum(3, seconds / 10);
-    matrix.writeDigitNum(1, minutes % 10);
-    matrix.writeDigitNum(0, minutes / 10);
-    matrix.writeDisplay();
+  matrix.drawColon(true);
+  matrix.writeDigitNum(4, seconds % 10);
+  matrix.writeDigitNum(3, seconds / 10);
+  matrix.writeDigitNum(1, minutes % 10);
+  matrix.writeDigitNum(0, minutes / 10);
+  matrix.writeDisplay();
 }
 
 // this method makes a HTTP connection to the server:
@@ -117,7 +61,6 @@ void httpRequest() {
 
   // if there's a successful connection:
   if (client.connect(server, 80)) {
-    Serial.println("connecting...");
     // send the HTTP PUT request:
     client.println("GET /latest.txt HTTP/1.1");
     client.println("Host: www.arduino.cc");
@@ -125,13 +68,8 @@ void httpRequest() {
     client.println("Connection: close");
     client.println();
 
-    // note the time that the connection was made:
-    lastConnectionTime = millis();
   }
-  else {
-    // if you couldn't make a connection:
-    Serial.println("connection failed");
-  }
+
 }
 
 
